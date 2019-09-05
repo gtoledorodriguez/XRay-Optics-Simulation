994c61a1f83195d35dae411f78825909bd90d3dc
Henry Post <hpost@hawk.iit.edu>
2019-09-05 13:25:34 -0500

    rename arg so as to not shadow Grating class from outer scope

--- src/gratingLib/Grating.py
+++ src/gratingLib/Grating.py
@@ -38,110 +38,110 @@ class Grating:
                     source.amplitude = newAmplitude
 
 
-def makeSlits(Grating, slit_width, slit_height, num_sources, source_spacing):
+def makeSlits(grating, slit_width, slit_height, num_sources, source_spacing):
     # This function will create a set amount of slits based on the amount of slits a Grating class wants. Each slit is created with
     # 'num_sources' number of sources with a slit width of 'slit_width.' Depending on the amount of sources a grating wants, this
     # function sets up different diffraction scenarios, like single slit diffraction, double slit diffraction, and Grating
     # diffraction
 
-    if Grating.numberOfSlits == 1:
+    if grating.numberOfSlits == 1:
         # Modeling Single Slit Diffraction
 
         # place a slit in the middle of the Grating.
         # Note: the y position of a slit is defined as its endpoint closest to the x axis
-        center = Grating.length / 2
-        thisSlit = [Slit(Grating.x, center - slit_width / 2, slit_width, num_sources, [])] * slit_height
+        center = grating.length / 2
+        thisSlit = [Slit(grating.x, center - slit_width / 2, slit_width, num_sources, [])] * slit_height
         # thisSlit = Slit(Grating.x, center - slit_width/2, slit_width, num_sources, [])
-        Grating.slits.append(thisSlit)
+        grating.slits.append(thisSlit)
         makeSources(thisSlit, slit_height, 0, source_spacing)
 
-        for slit in Grating.slits:
+        for slit in grating.slits:
             for i in range(0, slit_height - 1):
                 for sources in slit[i].sources:
-                    Grating.pointSourcePositions.append(source.y)
-                    Grating.pointSourceAmplitudes.append(source.amplitude)
+                    grating.pointSourcePositions.append(source.y)
+                    grating.pointSourceAmplitudes.append(source.amplitude)
 
-    elif Grating.numberOfSlits == 2:
+    elif grating.numberOfSlits == 2:
         # Modeling Double Slit Diffraction
         # place two slits, with one 'slit_width' of distance between them
 
-        center = Grating.length / 2
-        slit1 = [Slit(Grating.x, center - slit_width / 2, slit_width, num_sources, [])] * slit_height
-        slit2 = [Slit(Grating.x, center - slit_width / 2, slit_width, num_sources, [])] * slit_height
+        center = grating.length / 2
+        slit1 = [Slit(grating.x, center - slit_width / 2, slit_width, num_sources, [])] * slit_height
+        slit2 = [Slit(grating.x, center - slit_width / 2, slit_width, num_sources, [])] * slit_height
         # slit1 = Slit(Grating.x, center - slit_width*1.5, slit_width, num_sources, [])
         # slit2 = Slit(Grating.x, center + slit_width*1.5, slit_width, num_sources, [])
-        Grating.slits.append(slit1)
-        Grating.slits.append(slit2)
+        grating.slits.append(slit1)
+        grating.slits.append(slit2)
 
-        for slit in Grating.slits:
+        for slit in grating.slits:
             makeSources(slit, slit_height, 0, source_spacing)
 
-        for slit in Grating.slits:
+        for slit in grating.slits:
             for i in range(0, slit_height - 1):
                 for source in slit[i].sources:
-                    Grating.pointSourcePositions.append(source.y)
-                    Grating.pointSourceAmplitudes.append(source.amplitude)
+                    grating.pointSourcePositions.append(source.y)
+                    grating.pointSourceAmplitudes.append(source.amplitude)
 
-    elif Grating.numberOfSlits > 2:
+    elif grating.numberOfSlits > 2:
         # Modeling a Grating with numberOfSlits
         # this Grating is expected to have at least a slit_width of distance between each slit
         # numberOfSlits is a variable input and each should start from the center and built outwards
 
-        if Grating.numberOfSlits % 2 == 0:
+        if grating.numberOfSlits % 2 == 0:
 
-            center = Grating.length / 2
+            center = grating.length / 2
 
             i = 0
 
-            while Grating.numberOfSlits / 2 > i:
+            while grating.numberOfSlits / 2 > i:
                 slit1_y = center + slit_width * 0.5 + 2 * i * slit_width
                 slit2_y = center - slit_width * 1.5 - 2 * i * slit_width
 
                 # Made slit 2 dimentional by making an array of slit height size, with identical slits.
-                slit_1 = [Slit(Grating.x, slit1_y, slit_width, num_sources, [])] * slit_height
-                slit_2 = [Slit(Grating.x, slit2_y, slit_width, num_sources, [])] * slit_height
-                Grating.slits.append(slit_1)
-                Grating.slits.append(slit_2)
+                slit_1 = [Slit(grating.x, slit1_y, slit_width, num_sources, [])] * slit_height
+                slit_2 = [Slit(grating.x, slit2_y, slit_width, num_sources, [])] * slit_height
+                grating.slits.append(slit_1)
+                grating.slits.append(slit_2)
 
                 i += 1
 
-            for slit in Grating.slits:
+            for slit in grating.slits:
                 makeSources(slit, slit_height, 0, source_spacing)
 
-            for slit in Grating.slits:
+            for slit in grating.slits:
                 for i in range(0, slit_height - 1):
                     for source in slit[i].sources:
-                        Grating.pointSourcePositions.append(source.y)
-                        Grating.pointSourceAmplitudes.append(source.amplitude)
+                        grating.pointSourcePositions.append(source.y)
+                        grating.pointSourceAmplitudes.append(source.amplitude)
 
         else:
-            center = Grating.length / 2
+            center = grating.length / 2
 
-            first_slit = Slit(Grating.x, center - slit_width / 2, slit_width, num_sources, [])
-            Grating.slits.append(first_slit)
+            first_slit = Slit(grating.x, center - slit_width / 2, slit_width, num_sources, [])
+            grating.slits.append(first_slit)
 
             i = 0
 
-            while (Grating.numberOfSlits - 1) / 2 > i:
+            while (grating.numberOfSlits - 1) / 2 > i:
                 slit1_y = center + slit_width * 1.5 + 2 * i * slit_width
                 slit2_y = center - slit_width * 2.5 - 2 * i * slit_width
 
                 # Made slit 2 dimentional by making an array of slit height size, with identical slits.
-                slit_1 = [Slit(Grating.x, slit1_y, slit_width, num_sources, [])] * slit_height
-                slit_2 = [Slit(Grating.x, slit2_y, slit_width, num_sources, [])] * slit_height
-                Grating.slits.append(slit_1)
-                Grating.slits.append(slit_2)
+                slit_1 = [Slit(grating.x, slit1_y, slit_width, num_sources, [])] * slit_height
+                slit_2 = [Slit(grating.x, slit2_y, slit_width, num_sources, [])] * slit_height
+                grating.slits.append(slit_1)
+                grating.slits.append(slit_2)
 
                 i += 1
 
-            for slit in Grating.slits:
+            for slit in grating.slits:
                 makeSources(slit, slit_height, 0, source_spacing)
 
-            for slit in Grating.slits:
+            for slit in grating.slits:
                 # for i in range(0, slit_height -1):
                 for source in slit[i].sources:
-                    Grating.pointSourcePositions.append(source.y)
-                    Grating.pointSourceAmplitudes.append(source.amplitude)
+                    grating.pointSourcePositions.append(source.y)
+                    grating.pointSourceAmplitudes.append(source.amplitude)
 
 
 def makeSources(Slit, SlitHeight, amplitude, spacing_type):
