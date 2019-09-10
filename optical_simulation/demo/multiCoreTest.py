5cbf6862a615fe2e743f400a6d01fb463ebfa056
Michael Elnajami <makmoud98@gmail.com>
2019-09-10 14:37:39 -0500

    refactored/cleaned up multicore test

--- optical_simulation/demo/multiCoreTest.py
+++ optical_simulation/demo/multiCoreTest.py
@@ -17,130 +17,42 @@ def run_multicore_test(cpu_cores: int = 8, number_calculations: int = 1000000, s
 
     # Initialize arrays based on arguments provided
     q = Queue()  # used to save information generated within child process in separate core
-    array1 = []
-    array2 = []
+    
+    calculations_per_core = int(number_calculations / cpu_cores)  # Splits val into [numberCores] parts, which will need to merge later
+    # create an array of arrays
+    jobs = cpu_cores*[list(range(calculations_per_core))]
 
-    if cpu_cores > 2:
-        array3 = []
-    if cpu_cores > 3:
-        array4 = []
-    if cpu_cores > 4:
-        array5 = []
-    if cpu_cores > 5:
-        array6 = []
-    if cpu_cores > 6:
-        array7 = []
-    if cpu_cores > 7:
-        array8 = []
-
-    # Independent functions to fill arrays within separate cores with corresponding values
-
-    multiVal = int(
-        number_calculations / cpu_cores)  # Splits val into [numberCores] parts, which will need to merge later
-
-    def generate_core_fn(core_number, work_array: List[List[int]], multiVal) -> Callable[[int], None]:
+    # Independent functions to fill arrays within separate cores with corresponding values  
+    def generate_core_fn(core_number, work_array: List[List[int]], calculations_per_core) -> Callable[[int], None]:
         """Generate a function for a core to run. TODO"""
 
-        def core_n(_):
-            for i in range(0, multiVal):
-                work_array.append(i)
+        def core_n():
+            for i in range(calculations_per_core):
+                work_array[i] = i
+            q.put(work_array)
             pass
 
         return core_n
 
         pass
 
-    def core1(n):
-        for i in range(0, multiVal):
-            array1.append(i)
-        q.put(array1)
 
-    def core2(n):
-        for i in range(0, multiVal):
-            array2.append(i + multiVal)
-        q.put(array2)
-
-    def core3(n):
-        for i in range(0, multiVal):
-            array3.append(i + (multiVal * 2))
-        q.put(array3)
-
-    def core4(n):
-        for i in range(0, int(number_calculations / cpu_cores)):
-            array4.append(i + (multiVal * 3))
-        q.put(array4)
-
-    def core5(n):
-        for i in range(0, multiVal):
-            array5.append(i + (multiVal * 4))
-        q.put(array5)
-
-    def core6(n):
-        for i in range(0, multiVal):
-            array6.append(i + (multiVal * 5))
-        q.put(array6)
-
-    def core7(n):
-        for i in range(0, multiVal):
-            array7.append(i + (multiVal * 6))
-        q.put(array7)
-
-    def core8(n):
-        for i in range(0, multiVal):
-            array8.append(i + (multiVal * 7))
-        q.put(array8)
+    core_functions = list(map(lambda x: generate_core_fn( x, jobs[x], len(jobs[x]) ), range(cpu_cores)))
 
     # Separate processeses between [numberCores] cores
+        
+    threads = []
+    
+    for i in range(cpu_cores):
+        threads.append(multiprocessing.Process(target=core_functions[i]))
 
-    for i in range(1):  # Scaled to create between 2 to 8 cores based on args provided
-        t = multiprocessing.Process(target=core1, args=(i,))
-        t2 = multiprocessing.Process(target=core2, args=(i,))
-        if cpu_cores > 2:
-            t3 = multiprocessing.Process(target=core3, args=(i,))
-        if cpu_cores > 3:
-            t4 = multiprocessing.Process(target=core4, args=(i,))
-        if cpu_cores > 4:
-            t5 = multiprocessing.Process(target=core5, args=(i,))
-        if cpu_cores > 5:
-            t6 = multiprocessing.Process(target=core6, args=(i,))
-        if cpu_cores > 6:
-            t7 = multiprocessing.Process(target=core7, args=(i,))
-        if cpu_cores > 7:
-            t8 = multiprocessing.Process(target=core8, args=(i,))
+    for thread in threads:
+        thread.daemon = True
 
-        # 'daemon = true' stops the parent process from having to wait for child processes to end before terminating.
-        t.daemon = True
-        t2.daemon = True
-        if cpu_cores > 2:
-            t3.daemon = True
-        if cpu_cores > 3:
-            t4.daemon = True
-        if cpu_cores > 4:
-            t5.daemon = True
-        if cpu_cores > 5:
-            t6.daemon = True
-        if cpu_cores > 6:
-            t7.daemon = True
-        if cpu_cores > 7:
-            t8.daemon = True
-
-        # START TIMER, run processes
-        start = time.time()
-
-        t.start()  # start processes
-        t2.start()
-        if cpu_cores > 2:
-            t3.start()
-        if cpu_cores > 3:
-            t4.start()
-        if cpu_cores > 4:
-            t5.start()
-        if cpu_cores > 5:
-            t6.start()
-        if cpu_cores > 6:
-            t7.start()
-        if cpu_cores > 7:
-            t8.start()
+    # START TIMER, run processes
+    start = time.time()
+    for thread in threads:
+        thread.start()
 
     mylist = []  # Used to retrieve values from queue sequentially
 
