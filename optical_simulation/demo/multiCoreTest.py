3e768428c8dd39431aef053a4d538b6239bd3e00
Michael Elnajami <makmoud98@gmail.com>
2019-09-12 13:41:02 -0500

    fixed race condition + removed prints to increase perf

--- optical_simulation/demo/multiCoreTest.py
+++ optical_simulation/demo/multiCoreTest.py
@@ -20,7 +20,7 @@ def run_multicore_test(cpu_cores: int = 8, number_calculations: int = 1000000, s
     
     calculations_per_core = int(number_calculations / cpu_cores)  # Splits val into [numberCores] parts, which will need to merge later
     # create an array of arrays
-    jobs = cpu_cores*[list(range(calculations_per_core))]
+    jobs = cpu_cores*[cpu_cores*[]]
 
     # Independent functions to fill arrays within separate cores with corresponding values  
     def generate_core_fn(core_number, work_array: List[List[int]], calculations_per_core) -> Callable[[int], None]:
@@ -28,9 +28,9 @@ def run_multicore_test(cpu_cores: int = 8, number_calculations: int = 1000000, s
 
         def core_n():
             for i in range(calculations_per_core):
-                work_array[i] = i
+                work_array.append(i)
             q.put(work_array)
-            print("core {}: job done".format(core_number))
+            #print("Core {}: Job Done".format(core_number))
             pass
 
         return core_n
@@ -55,23 +55,15 @@ def run_multicore_test(cpu_cores: int = 8, number_calculations: int = 1000000, s
     for thread in threads:
         thread.start()
 
-
-    mylist = []  # Used to retrieve values from queue sequentially
-
-    print("Sleeping for {} seconds.".format(sleep_time))
-    #time.sleep(sleep_time)  # FIXME: This is a race condition!
     for i in range(len(threads)):
-        print("Joining thread " + str(i))
         if threads[i].is_alive():
+            #print("Joining thread " + str(i))
             threads[i].join()
-            print('done joining')
 
     # Get objects in queue
     if q.empty():
         print("\nFail: Timer too short to allow child processes enough time to join parent\nProgram terminated\n")
         sys.exit(0)
-    # while not q.empty():
-    #   mylist.append(q.get())
 
     # Merge objects from queue into a single array
     mergedArray = []
@@ -88,10 +80,11 @@ def run_multicore_test(cpu_cores: int = 8, number_calculations: int = 1000000, s
     newstart = time.time()
     for j in range(0, number_calculations):
         array.append(j)
+
     time.sleep(sleep_time)
     # print("\nTotal time single: ")
     singleTime = (time.time() - newstart) - sleep_time
     # print(singleTime)
 
-    # print("\nEfficiency ratio:\n",(multiTime/singleTime), "\n")
+    print("\nEfficiency ratio:\n",(multiTime/singleTime), "\n")
     return (multiTime, singleTime)
