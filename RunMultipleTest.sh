5f31fea64ee407de23f2876bbeae35253f0e6b44
Henry Post <hpost@hawk.iit.edu>
2019-09-28 13:09:18 -0500

    use updated cli interface

--- RunMultipleTest.sh
+++ RunMultipleTest.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-#python -m optical_simulation.2GratingDiffraction_final --imageSubdir=slitHeight1 --slitHeight=
-#python -m optical_simulation.2GratingDiffraction_final --imageSubdir=slitHeight2 --slitHeight=2
-#python -m optical_simulation.2GratingDiffraction_final --imageSubdir=slitHeight3 --slitHeight=3
+#python -m optical_simulation.2GratingDiffraction_final --imageSubdirs slitHeight1 --slitHeight=1
+#python -m optical_simulation.2GratingDiffraction_final --imageSubdirs slitHeight2 --slitHeight=2
+#python -m optical_simulation.2GratingDiffraction_final --imageSubdirs slitHeight3 --slitHeight=3
 
 
 for (( n=0; n<=100; n+=10)); do
-    echo "$n"
 
-    python -m optical_simulation.2GratingDiffraction_final --imageSubdir="numOfPointSources${n}" --numOfPointSources="${n}"
-    ls $n
+    python -m optical_simulation.2GratingDiffraction_final --imageSubdirs "numOfPointSources${n}" --numOfPointSources="${n}"
 
 done
 
