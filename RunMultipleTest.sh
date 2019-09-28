6d63b3d814a14b82de9333587371f2781cd0c8f7
Henry Post <hpost@hawk.iit.edu>
2019-09-28 13:48:09 -0500

    comment script

--- RunMultipleTest.sh
+++ RunMultipleTest.sh
@@ -4,16 +4,21 @@
 #python -m optical_simulation.2GratingDiffraction_final --imageSubdirs slitHeight2 --slitHeight=2
 #python -m optical_simulation.2GratingDiffraction_final --imageSubdirs slitHeight3 --slitHeight=3
 
+
 if ! [[ -d "./optical_simulation/image_output/pointSource_x_obsPoints" ]]; then
+
+    # From 0 to 100 step 10 in point sources,
     for (( point_src=0; point_src<=100; point_src+=10)); do
+
+        # From 0 to 1000 step 100 in observation points,
         for (( obs_point=0; obs_point<=1000; obs_point+=100 )); do
+
+            # Run the simulation with two variables.
             python -m optical_simulation.2GratingDiffraction_final --imageSubdirs "pointSource_x_obsPoints" "pointSource${point_src}_x_obsPoint${obs_point}" \
                 --numOfPointSources="${point_src}" --numObsPoints="${obs_point}"
         done
     done
 fi
-
-
 
 # If we haven't ran the 'numOfPointSourcesRuns' simulations, then run a LARGE amount of those simulations and create that directory.
 if ! [[ -d "./optical_simulation/image_output/numOfPointSourcesRuns" ]]; then
