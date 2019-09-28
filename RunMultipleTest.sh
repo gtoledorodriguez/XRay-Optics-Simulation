f6b1ceb7b19d5b75d6182b75fe426082d5ffe4dc
Henry Post <hpost@hawk.iit.edu>
2019-09-28 13:19:50 -0500

    check right dir

--- RunMultipleTest.sh
+++ RunMultipleTest.sh
@@ -6,14 +6,14 @@
 
 
 # If we haven't ran the 'numOfPointSourcesRuns' simulations, then run a LARGE amount of those simulations and create that directory.
-if ! [[ -d "./image_output/numOfPointSourcesRuns" ]]; then
+if ! [[ -d "./optical_simulation/image_output/numOfPointSourcesRuns" ]]; then
     for (( n=0; n<=100; n+=10)); do
         python -m optical_simulation.2GratingDiffraction_final --imageSubdirs "numOfPointSourcesRuns" "numOfPointSources${n}" --numOfPointSources="${n}"
     done
 fi
 
 # If we haven't ran the 'numObsPoints' simulations, then run a LARGE amount of those simulations and create that directory.
-if ! [[ -d "./image_output/numObsPointsRuns/" ]]; then
+if ! [[ -d "./optical_simulation/image_output/numObsPointsRuns/" ]]; then
     for (( n=0; n<=1000; n+=50 )); do
         python -m optical_simulation.2GratingDiffraction_final --imageSubdirs "numObsPointsRuns" "numObsPoints${n}" --numObsPoints="${n}"
     done
