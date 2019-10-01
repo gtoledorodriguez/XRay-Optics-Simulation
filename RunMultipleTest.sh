163c88f0cd8c6456f63901d3289c4e6d08992de2
Henry Post <HenryFBP@gmail.com>
2019-10-01 14:32:23 -0500

    add echo for folder existing

--- RunMultipleTest.sh
+++ RunMultipleTest.sh
@@ -10,6 +10,8 @@ if ! [[ -d "./optical_simulation/image_output/timingTests" ]]; then
     for (( n=0; n<=5; n+=1)); do
         python -m optical_simulation.2GratingDiffraction_final --imageSubdirs "timingTests" "timingTest${n}"
     done
+else
+    echo "Timing tests already ran. Delete the folder to run them again."
 fi
 
 if ! [[ -d "./optical_simulation/image_output/pointSource_x_obsPoints" ]]; then
@@ -26,6 +28,8 @@ if ! [[ -d "./optical_simulation/image_output/pointSource_x_obsPoints" ]]; then
                 --numOfPointSources="${point_src}" --numObsPoints="${obs_point}"
         done
     done
+else
+    echo "Point source number + observation point number tests already ran. Delete the folder to run them again."
 fi
 
 # If we haven't ran the 'numOfPointSourcesRuns' simulations, then run a LARGE amount of those simulations and create that directory.
@@ -34,6 +38,8 @@ if ! [[ -d "./optical_simulation/image_output/numOfPointSourcesRuns" ]]; then
     for (( n=0; n<=1000; n+=100)); do
         python -m optical_simulation.2GratingDiffraction_final --imageSubdirs "numOfPointSourcesRuns" "numOfPointSources${n}" --numOfPointSources="${n}"
     done
+else
+    echo "Point source tests already ran. Delete the folder to run them again."
 fi
 
 # If we haven't ran the 'numObsPoints' simulations, then run a LARGE amount of those simulations and create that directory.
@@ -41,6 +47,8 @@ if ! [[ -d "./optical_simulation/image_output/numObsPointsRuns/" ]]; then
     for (( n=0; n<=1000; n+=50 )); do
         python -m optical_simulation.2GratingDiffraction_final --imageSubdirs "numObsPointsRuns" "numObsPoints${n}" --numObsPoints="${n}"
     done
+else
+    echo "Observation point tests already ran. Delete the folder to run them again."
 fi
 
 num=$(awk 'BEGIN{for(i=0; i<=10; i+=0.25)print i}')
@@ -49,4 +57,6 @@ if ! [[ -d "./optical_simulation/image_output/U_0/" ]]; then
     for n in $num; do
         python -m optical_simulation.2GratingDiffraction_final --imageSubdirs "U_0" "u_0${n}" --U_0="${n}"
     done
+else
+    echo "U_0 tests already ran. Delete the folder to run them again."
 fi
