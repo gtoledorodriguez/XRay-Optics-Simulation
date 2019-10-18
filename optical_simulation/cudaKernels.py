e455fb1ce2fadae709050f0a6b98a09d1d59892b
Michael Elnajami <makmoud98@gmail.com>
2019-10-18 16:51:45 -0500

    added memory/kernel benchmark to/from gpu

--- optical_simulation/cudaKernels.py
+++ optical_simulation/cudaKernels.py
@@ -11,6 +11,8 @@
     Alec Buchanan - 03/2018
 """
 
+from timeit import default_timer as timer
+
 import cmath
 import math
 
@@ -45,10 +47,10 @@ def intensityKernel(GratingSeparation, WaveNumber, sourcePoints, obsPoints, sour
       Alec Buchanan - 3/2018
     """
     # Basic CUDA code to determine which thread we are in
-    tx = cuda.threadIdx.x  # Thread id in a 1D block = particle index
-    ty = cuda.blockIdx.x  # Block id in a 1D grid = event index
-    bw = cuda.blockDim.x  # Block width, i.e. number of threads per block = particle number
-    pos = tx + ty * bw  # computed flattened index inside the array
+    #tx = cuda.threadIdx.x  # Thread id in a 1D block = particle index
+    #ty = cuda.blockIdx.x  # Block id in a 1D grid = event index
+    #bw = cuda.blockDim.x  # Block width, i.e. number of threads per block = particle number
+    pos = cuda.grid(1)  # computed flattened index inside the array
 
     # initialize variables 
     phaseSum = 0
@@ -122,10 +124,57 @@ def intensityCalculations(GratingSeparation, WaveNumber, sourcePoints, obsPoints
     out_a = np.array(out_a, dtype='f4')
     out_i = np.array(out_i, dtype='f4')
 
-    # call CUDA kernel
-    intensityKernel[blockspergrid, threadsperblock](GratingSeparation, WaveNumber, sourcePoints, obsPoints, sourceAmp,
-                                                    sourcePhase, out_p, out_a, out_i)
 
+    evt_total_begin = cuda.event()
+    evt_total_end = cuda.event()
+
+    evt_mem_begin = cuda.event()
+    evt_mem_end = cuda.event()
+
+    evt_mem2_begin = cuda.event()
+    evt_mem2_end = cuda.event()
+
+    evt_kernel_begin = cuda.event()
+    evt_kernel_end = cuda.event()
+
+    evt_total_begin.record()
+    evt_mem_begin.record()
+
+    d_sourcePoints = cuda.to_device(sourcePoints)
+    d_obsPoints = cuda.to_device(obsPoints)
+    d_sourceAmp = cuda.to_device(sourceAmp)
+    d_sourcePhase = cuda.to_device(sourcePhase)
+    d_out_i = cuda.to_device(out_i)
+    d_out_a = cuda.to_device(out_a)
+    d_out_p = cuda.to_device(out_p)
+
+    evt_mem_end.record()
+    evt_mem_end.synchronize()
+
+    evt_kernel_begin.record()
+    # call CUDA kernel
+    intensityKernel[blockspergrid, threadsperblock](GratingSeparation, WaveNumber, d_sourcePoints, d_obsPoints, d_sourceAmp,
+                                                    d_sourcePhase, d_out_p, d_out_a, d_out_i)
+
+    evt_kernel_end.record()
+    evt_kernel_end.synchronize()
+    
+    evt_mem2_begin.record()
+
+    out_i = d_out_i.copy_to_host()
+    out_a= d_out_a.copy_to_host()
+    out_p = d_out_p.copy_to_host()
+
+    evt_mem2_end.record()
+    evt_mem2_end.synchronize()
+
+    evt_total_end.record()
+    evt_total_end.synchronize()
+
+    print('total time: %fms' % evt_total_begin.elapsed_time(evt_total_end))
+    print('mem-to-gpu time: %fms' % evt_mem_begin.elapsed_time(evt_mem_end))
+    print('kernel time: %fms' % evt_kernel_begin.elapsed_time(evt_kernel_end))
+    print('mem-from-gpu time: %fms' % evt_mem2_begin.elapsed_time(evt_mem2_end))
     # Remove Imaginary parts
     out_i = np.array(out_i, dtype='f4')
     out_a = np.array(out_a, dtype='f4')
