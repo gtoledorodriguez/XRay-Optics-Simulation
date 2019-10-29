56d3a2713c58aa078c7685fd0ffd8bac40c281f0
Henry Post <hpost@hawk.iit.edu>
2019-10-29 13:55:59 -0500

    update path from new git location

--- profile-visual.sh
+++ profile-visual.sh
@@ -5,7 +5,7 @@ source ./options.sh # Include file.
 echo "Use these settings for nvvp:"
 echo "Toolkit/Script: CUDA Toolkit 9.1 (/usr/local/cuda-9.1/bin/)"
 echo "File: /home/YOUR_USERNAME/miniconda3/envs/agis/bin/python3.7"
-echo "Working directory: /home/YOUR_USERNAME/GitHub/Antimatter-Gravity-Interferometer-Simulation/"
+echo "Working directory: /home/YOUR_USERNAME/GitHub/XRay-Optics-Simulation/"
 echo "Arguments: '-m optical_simulation.run_simulation'"
 
 sleep 3
