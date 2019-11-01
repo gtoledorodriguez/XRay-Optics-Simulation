57c8601b11ebe8bf893daba9fa6586d3e6a2326c
Henry Post <hpost@hawk.iit.edu>
2019-11-01 17:52:10 -0500

    start making csv

--- profile-gpu-memory-tests.sh
+++ profile-gpu-memory-tests.sh
@@ -17,11 +17,12 @@ for (( i = 0; i < ${#POINTSOURCES[@]}; ++i )); do
     numpointsource=${POINTSOURCES[i]}
 
     OUT_FILENAME="${OUT_DIR}/${numpointsource}-point-source.txt"
+    OUT_PROFILE_NAME="${OUT_DIR}/${numpointsource}-point-source.prof"
 
     if [[ ! -f ${OUT_DIR}/${numpointsource}-point-source.txt ]]; then
         echo "Have not run GPU memory test for ${numpointsource} point sources yet. Running..."
 
-        ${CUDA_DIR}/nvprof python -m optical_simulation.run_simulation --numOfPointSources ${numpointsource} > ${OUT_FILENAME} 2>&1
+        ${CUDA_DIR}/nvprof --analysis-metrics --export-profile ${OUT_PROFILE_NAME} python -m optical_simulation.run_simulation --numOfPointSources ${numpointsource} > ${OUT_FILENAME} 2>&1
     else
         echo "Already run GPU memory test for ${numpointsource} point sources. Skipping. Delete file at ${OUT_FILENAME} to run again."
     fi
@@ -29,10 +30,22 @@ for (( i = 0; i < ${#POINTSOURCES[@]}; ++i )); do
 done
 
 # From results, generate CSV file
-echo "Generating CSV file at ${OUT_CSV}"
+echo "Generating CSV file at ${OUT_CSV}."
+echo "There is no way that I can find to fill the memory transferred to CPU<->GPU by hand. That will have to be done
+by opening the .prof files in the output directory."
+
+echo "total-time,mem-to-gpu-time,kernel-time,memory-consumed" > ${OUT_CSV}
+
 for f in ${OUT_DIR}/*-point-source.txt; do
-    echo ${f}
-    cat ${f} | grep "total time:" | head -1
-    cat ${f} | grep "mem-to-gpu time:" | head -1
-    cat ${f} | grep "kernel time:" | head -1
+    aline=$(cat ${f} | grep "total time:" | head -1 | grep -P '\\d+?\\.')
+    aline+=","
+
+    aline+=$(cat ${f} | grep "mem-to-gpu time:" | head -1)
+    aline+=","
+    aline+=$(cat ${f} | grep "kernel time:" | head -1)
+    aline+=","
+
+    echo ${aline}
+    echo ${aline} >> ${OUT_CSV}
+
 done
