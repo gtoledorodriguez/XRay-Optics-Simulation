0b74f187ead1ef38f662378b922bc2e3777e209b
Henry Post <hpost@hawk.iit.edu>
2019-11-01 18:03:21 -0500

    make gpu memory profiling tests even better

--- profile-gpu-memory-tests.sh
+++ profile-gpu-memory-tests.sh
@@ -12,6 +12,8 @@ mkdir -p ${OUT_DIR}
 # List of point sources to try
 POINTSOURCES=(100 200 300)
 
+SLITHEIGHT=100 #15000 is realistic.
+
 # Generate results
 for (( i = 0; i < ${#POINTSOURCES[@]}; ++i )); do
     numpointsource=${POINTSOURCES[i]}
@@ -22,7 +24,7 @@ for (( i = 0; i < ${#POINTSOURCES[@]}; ++i )); do
     if [[ ! -f ${OUT_DIR}/${numpointsource}-point-source.txt ]]; then
         echo "Have not run GPU memory test for ${numpointsource} point sources yet. Running..."
 
-        ${CUDA_DIR}/nvprof --analysis-metrics --export-profile ${OUT_PROFILE_NAME} python -m optical_simulation.run_simulation --numOfPointSources ${numpointsource} > ${OUT_FILENAME} 2>&1
+        ${CUDA_DIR}/nvprof --analysis-metrics --export-profile ${OUT_PROFILE_NAME} python -m optical_simulation.run_simulation --slitHeight ${SLITHEIGHT} --numOfPointSources ${numpointsource} > ${OUT_FILENAME} 2>&1
     else
         echo "Already run GPU memory test for ${numpointsource} point sources. Skipping. Delete file at ${OUT_FILENAME} to run again."
     fi
@@ -34,18 +36,28 @@ echo "Generating CSV file at ${OUT_CSV}."
 echo "There is no way that I can find to fill the memory transferred to CPU<->GPU by hand. That will have to be done
 by opening the .prof files in the output directory."
 
-echo "total-time,mem-to-gpu-time,kernel-time,memory-consumed" > ${OUT_CSV}
+echo "sourcePoints,total-time (ms),mem-to-gpu-time (ms),kernel-time (ms),memory-transferred" > ${OUT_CSV}
 
 for f in ${OUT_DIR}/*-point-source.txt; do
-    aline=$(cat ${f} | grep "total time:" | head -1 | grep -P '\\d+?\\.')
+
+    aline=""
+
+    aline+=$(echo "${f}" | tr -dc '0-9')
     aline+=","
 
-    aline+=$(cat ${f} | grep "mem-to-gpu time:" | head -1)
-    aline+=","
-    aline+=$(cat ${f} | grep "kernel time:" | head -1)
+    aline+=$(cat ${f} | grep "total time:" | head -1 | tr -dc '0-9.')
     aline+=","
 
-    echo ${aline}
+    aline+=$(cat ${f} | grep "mem-to-gpu time:" | head -1 | tr -dc '0-9.')
+    aline+=","
+
+    aline+=$(cat ${f} | grep "kernel time:" | head -1 | tr -dc '0-9.')
+    aline+=","
+
+    aline+="REPLACE ME; see the .prof files. I don't know how to extract how much memory is transferred via command line."
+
     echo ${aline} >> ${OUT_CSV}
 
 done
+
+cat ${OUT_CSV}
